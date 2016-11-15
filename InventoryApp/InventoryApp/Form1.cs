a5995160e6ab82fdc70390328c17cd5ee8abb35f
Joel Gilbert <gilbert.279@gmail.com>
2016-11-14 20:51:31 -0600

    node removal

--- InventoryApp/InventoryApp/Form1.cs
+++ InventoryApp/InventoryApp/Form1.cs
@@ -8,15 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
 namespace InventoryApp
 {
     public partial class Form1 : Form
     {
+        BindingList<string> inventoryList = new BindingList<string>();
+
         public Form1()
         {
             InitializeComponent();
 
-            List<string> inventoryList = new List<string>(); 
+            //List<string> inventoryList = new List<string>(); 
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -36,19 +42,71 @@ namespace InventoryApp
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            string input = Console.ReadLine();
+            
 
         }
             
         private void button2_Click(object sender, EventArgs e)
         {
 
+            
+
+            inventoryList.Remove(inputBox.Text);
+            //listBox1.DataSource = null;
+            listBox1.DataSource = inventoryList;
+            xmlWrite();
+            nodeRemove();
+            error();
+
+            
+
         }
+      
 
         public void button3_Click(object sender, EventArgs e)
         {
-            inventoryList.Add(txtItem.Text);
-            txtItem.Text = "";
+                    
+                inventoryList.Add(inputBox.Text);        
+                //listBox1.DataSource = null;
+                listBox1.DataSource = inventoryList;
+                xmlWrite();
+                error();
+                
+        }
+        public void xmlWrite()
+        {
+            Stream stream = File.OpenWrite(Environment.CurrentDirectory + "\\File.xml");
+            XmlSerializer xmlSer = new XmlSerializer(typeof(BindingList<string>));
+            //We are now putting everything together.  We are using the Serialize process to pass our configuration and the list of person.
+            xmlSer.Serialize(stream, inventoryList);
+            //we need to close our stream
+            stream.Close();
+
+            Console.WriteLine(Environment.CurrentDirectory);
+        }
+        public void nodeRemove()
+        {
+        XmlDocument inventoryList = new XmlDocument();
+
+        inventoryList.Load(Environment.CurrentDirectory + "\\File.xml");
+
+        XmlNodeList list = inventoryList.SelectNodes("//*[not(string(.))]");
+            foreach (XmlNode node in list)
+            {
+                node.ParentNode.RemoveChild(node);
+            }
+         inventoryList.Save(Environment.CurrentDirectory + "\\File.xml");
+        }
+
+        public void error()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+            }
         }
     }
 }
